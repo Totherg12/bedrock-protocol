08a9cf7ed05e27df5d1f5b90517daac2ee6d3f10
Totherg12 <90158762+Totherg12@users.noreply.github.com>
2026-09-21 18:05:55 -0300

    Update options.js

--- src/options.js
+++ src/options.js
@@ -2,51 +2,143 @@ const mcData = require('minecraft-data')
 
 // Minimum supported version (< will be kicked)
 const MIN_VERSION = '1.16.201'
-// Currently supported verson. Note, clients with newer versions can still connect as long as data is in minecraft-data
+
+// Versão real dos dados internos disponíveis no minecraft-data
 const CURRENT_VERSION = '1.26.45'
 
-const Versions = Object.fromEntries(mcData.versions.bedrock.filter(e => e.releaseType === 'release').map(e => [e.minecraftVersion, e.version]))
+// Versão experimental do servidor
+const EXPERIMENTAL_VERSION = '1.26.51'
 
-// Skip some low priority versions (middle major) on Github Actions to allow faster CI
-const skippedVersionsOnGithubCI = ['1.16.210', '1.17.10', '1.17.30', '1.18.11', '1.19.10', '1.19.20', '1.19.30', '1.19.40', '1.19.50', '1.19.60', '1.19.63', '1.19.70', '1.20.10', '1.20.15', '1.20.30', '1.20.40', '1.20.50', '1.20.61', '1.20.71', '1.21.2', '1.21.20', '1.21.30', '1.21.42', '1.21.50', '1.21.60', '1.21.70', '1.21.80', '1.21.90', '1.21.93', '1.21.100', '1.21.111', '1.21.120', '1.21.124', '1.26.10', '1.26.20']
-const testedVersions = process.env.CI ? Object.keys(Versions).filter(v => !skippedVersionsOnGithubCI.includes(v)) : Object.keys(Versions)
+// ID do protocolo Bedrock 1.26.51
+const EXPERIMENTAL_PROTOCOL_VERSION = 2193
+
+const Versions = Object.fromEntries(
+  mcData.versions.bedrock
+    .filter(e => e.releaseType === 'release')
+    .map(e => [e.minecraftVersion, e.version])
+)
+
+// Skip some low priority versions on GitHub Actions
+const skippedVersionsOnGithubCI = [
+  '1.16.210',
+  '1.17.10',
+  '1.17.30',
+  '1.18.11',
+  '1.19.10',
+  '1.19.20',
+  '1.19.30',
+  '1.19.40',
+  '1.19.50',
+  '1.19.60',
+  '1.19.63',
+  '1.19.70',
+  '1.20.10',
+  '1.20.15',
+  '1.20.30',
+  '1.20.40',
+  '1.20.50',
+  '1.20.61',
+  '1.20.71',
+  '1.21.2',
+  '1.21.20',
+  '1.21.30',
+  '1.21.42',
+  '1.21.50',
+  '1.21.60',
+  '1.21.70',
+  '1.21.80',
+  '1.21.90',
+  '1.21.93',
+  '1.21.100',
+  '1.21.111',
+  '1.21.120',
+  '1.21.124',
+  '1.26.10',
+  '1.26.20'
+]
+
+const testedVersions = process.env.CI
+  ? Object.keys(Versions).filter(
+      version => !skippedVersionsOnGithubCI.includes(version)
+    )
+  : Object.keys(Versions)
 
 const defaultOptions = {
-  // https://minecraft.wiki/w/Protocol_version#Bedrock_Edition_2
   version: CURRENT_VERSION,
-  // client: If we should send SetPlayerInitialized to the server after getting play_status spawn.
-  // if this is disabled, no 'spawn' event will be emitted, you should manually set
-  // client.status to ClientStatus.Initialized after sending the init packet.
+
   autoInitPlayer: true,
-  // If true, do not authenticate with Xbox Live
+
   offline: false,
-  // Milliseconds to wait before aborting connection attempt
+
   connectTimeout: 9000,
-  // Specifies the raknet implementation to use
+
   raknetBackend: 'raknet-native',
-  // If using JS implementation of RakNet, should we use workers? (This only affects the client)
+
   useRaknetWorkers: true,
 
-  // server: What compression algorithm to use by default, either `none`, `deflate` or `snappy`
   compressionAlgorithm: 'deflate',
-  // server and client: On Deflate, what compression level to use, between 1 and 9
+
   compressionLevel: 7,
-  // server: If true, only compress if a payload is larger than compressionThreshold
+
   compressionThreshold: 512
 }
 
 function validateOptions (options) {
+  /*
+   * Suporte experimental para 1.26.51.
+   *
+   * A biblioteca usa os dados internos da 1.26.45,
+   * mas envia o número de protocolo da 1.26.51.
+   */
+  if (options.version === EXPERIMENTAL_VERSION) {
+    console.warn(
+      '[bedrock-protocol] Usando suporte experimental para Minecraft 1.26.51'
+    )
+
+    // Faz serializer/deserializer usarem os dados conhecidos da 1.26.45
+    options.version = CURRENT_VERSION
+
+    // Envia o protocolo da 1.26.51 ao servidor
+    options.protocolVersion = EXPERIMENTAL_PROTOCOL_VERSION
+
+    if (options.useNativeRaknet === true) {
+      options.raknetBackend = 'raknet-native'
+    }
+
+    if (options.useNativeRaknet === false) {
+      options.raknetBackend = 'jsp-raknet'
+    }
+
+    return
+  }
+
   if (!Versions[options.version]) {
     console.warn('Supported versions', Versions)
     throw Error(`Unsupported version ${options.version}`)
   }
 
   options.protocolVersion = Versions[options.version]
+
   if (options.protocolVersion < MIN_VERSION) {
-    throw new Error(`Protocol version < ${MIN_VERSION} : ${options.protocolVersion}, too old`)
+    throw new Error(
+      `Protocol version < ${MIN_VERSION}: ${options.protocolVersion}, too old`
+    )
   }
-  if (options.useNativeRaknet === true) options.raknetBackend = 'raknet-native'
-  if (options.useNativeRaknet === false) options.raknetBackend = 'jsp-raknet'
+
+  if (options.useNativeRaknet === true) {
+    options.raknetBackend = 'raknet-native'
+  }
+
+  if (options.useNativeRaknet === false) {
+    options.raknetBackend = 'jsp-raknet'
+  }
 }
 
-module.exports = { defaultOptions, MIN_VERSION, CURRENT_VERSION, Versions, validateOptions, testedVersions }
+module.exports = {
+  defaultOptions,
+  MIN_VERSION,
+  CURRENT_VERSION,
+  Versions,
+  validateOptions,
+  testedVersions
+}
